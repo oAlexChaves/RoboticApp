49ce1dceacb148778b5bc24d56e43a68b089a7b8
oAlexChaves <68710954+oAlexChaves@users.noreply.github.com>
2021-03-29 21:27:14 -0300

    aprendendo ionic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'alex-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
